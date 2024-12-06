5f675a6f5f731f0261cb6c23ac58f789ff330c13
hellenilda <hellen95limaaraujo@gmail.com>
2024-12-06 19:24:44 -0300

    :worm: fix: problema ao logar como adm

--- cardapio/index.js
+++ cardapio/index.js
@@ -4,15 +4,14 @@ import session from 'express-session';
 import path from 'path';
 import cryptoRandomString from 'crypto-random-string';
 import methodOverride from 'method-override';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 3000;
-
-// Supondo que você esteja exportando appRouter corretamente
-import appRouter from './routes/routes.js'; // Adicione a extensão .js
+import appRouter from './routes/routes.js';
 
 const app = express();
 
-// Configurando o Handlebars como template engine
 app.engine('handlebars', engine({
     defaultLayout: 'main',
     helpers: {
@@ -22,25 +21,35 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars');
 app.set('views', path.join('./', 'views'));
 
-// Gerar uma chave secreta
+app.use('/uploads', express.static(path.join('./', 'uploads')));
+
 const secreta = cryptoRandomString({ length: 32, type: 'base64' });
 console.log(secreta);
 
 app.use(session({
-    secret: secreta, // Use uma chave secreta forte para produção
+    secret: secreta,
     resave: false,
     saveUninitialized: true
 }));
 
-// Middleware para tratar dados de formulário
+app.use(cors({
+    origin: '*', //
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(methodOverride('_method'));
 
-// Roteamento
+// Configurando cookie-parser
+app.use(cookieParser());
+
+// Roteamento   
 app.use('/', appRouter);
 
+// Inicia o servidor em HTTP
 app.listen(port, () => {
-    console.log(`Aplicação rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
